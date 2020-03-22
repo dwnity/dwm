07265bf77aad4d51363dab77371fd4009f70a41e
Fuseteam <fusekai@outlook.com>
2020-03-21 22:36:58 -0300

    add keybinding for printscreen key

--- config.def.h
+++ config.def.h
@@ -55,10 +55,10 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ KeyPress,    MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ KeyPress,    MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ KeyPress,    MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ KeyPress,    MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -73,52 +73,54 @@ static const char *browser[]  = { "firefox", NULL };
 static void toggleemoji();
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_e,      toggleemoji,    {0} },
-	{ MODKEY|ShiftMask,             XK_r,	   spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = browser } },
-	{ MODKEY|ShiftMask,             XK_d,      setopacity,     {.f = -0.05} },
-	{ MODKEY|ShiftMask,             XK_i,      setopacity,     {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_f,      setopacity,     {.f = 2.0} },
-	{ MODKEY|ControlMask,		XK_d,	   spawn,	   SHCMD("pactl -- set-sink-volume 0 -10%") },
-	{ MODKEY|ControlMask,		XK_i, 	   spawn,	   SHCMD("pactl -- set-sink-volume 0 +10%") },
-	{ MODKEY|ControlMask,		XK_m, 	   spawn,	   SHCMD("amixer -D pulse set Master toggle") },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask|ControlMask, XK_d,      setborderpx,    {.i = -1 } },
-	{ MODKEY|ShiftMask|ControlMask, XK_i,      setborderpx,    {.i = +1 } },
-	{ MODKEY|ShiftMask|ControlMask, XK_f,      setborderpx,    {.i = 0 } },
-	{ MODKEY|ShiftMask|ControlMask, XK_m,      setborderpx,    {.i = 2 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/*  type       modifier                         key          function        argument */
+	{ KeyPress,    MODKEY,                          XK_r,        spawn,          {.v = dmenucmd } },
+	{ KeyPress,    MODKEY,                          XK_e,        toggleemoji,    {0} },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_r,	     spawn,          {.v = termcmd } },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_Return,   spawn,          {.v = browser } },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_d,        setopacity,     {.f = -0.05} },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_i,        setopacity,     {.f = +0.05} },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_f,        setopacity,     {.f = 2.0} },
+	{ KeyPress,    0,                               XK_Print,    spawn,          SHCMD("cd Pictures/screenshots;scrot") },
+	{ KeyRelease,  ControlMask,                     XK_Print,    spawn,          SHCMD("cd Pictures/screenshots;scrot -s") },
+	{ KeyPress,    MODKEY|ControlMask,              XK_d,        spawn,	    SHCMD("pactl -- set-sink-volume 0 -10%") },
+	{ KeyPress,    MODKEY|ControlMask,              XK_i,        spawn,	    SHCMD("pactl -- set-sink-volume 0 +10%") },
+	{ KeyPress,    MODKEY|ControlMask,              XK_m,        spawn,	    SHCMD("amixer -D pulse set Master toggle") },
+	{ KeyPress,    MODKEY,                          XK_b,        togglebar,      {0} },
+	{ KeyPress,    MODKEY,                          XK_j,        focusstack,     {.i = +1 } },
+	{ KeyPress,    MODKEY,                          XK_k,        focusstack,     {.i = -1 } },
+	{ KeyPress,    MODKEY,                          XK_i,        incnmaster,     {.i = +1 } },
+	{ KeyPress,    MODKEY,                          XK_d,        incnmaster,     {.i = -1 } },
+	{ KeyPress,    MODKEY,                          XK_h,        setmfact,       {.f = -0.05} },
+	{ KeyPress,    MODKEY,                          XK_l,        setmfact,       {.f = +0.05} },
+	{ KeyPress,    MODKEY,                          XK_Return,   zoom,           {0} },
+	{ KeyPress,    MODKEY,                          XK_Tab,      view,           {0} },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_c,        killclient,     {0} },
+	{ KeyPress,    MODKEY,                          XK_t,        setlayout,      {.v = &layouts[0]} },
+	{ KeyPress,    MODKEY,                          XK_f,        setlayout,      {.v = &layouts[1]} },
+	{ KeyPress,    MODKEY,                          XK_m,        setlayout,      {.v = &layouts[2]} },
+	{ KeyPress,    MODKEY,                          XK_space,    setlayout,      {0} },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_space,    togglefloating, {0} },
+	{ KeyPress,    MODKEY,                          XK_0,        view,           {.ui = ~0 } },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_0,        tag,            {.ui = ~0 } },
+	{ KeyPress,    MODKEY,                          XK_comma,    focusmon,       {.i = -1 } },
+	{ KeyPress,    MODKEY,                          XK_period,   focusmon,       {.i = +1 } },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_comma,    tagmon,         {.i = -1 } },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_period,   tagmon,         {.i = +1 } },
+	{ KeyPress,    MODKEY|ShiftMask|ControlMask,    XK_d,        setborderpx,    {.i = -1 } },
+	{ KeyPress,    MODKEY|ShiftMask|ControlMask,    XK_i,        setborderpx,    {.i = +1 } },
+	{ KeyPress,    MODKEY|ShiftMask|ControlMask,    XK_f,        setborderpx,    {.i = 0 } },
+	{ KeyPress,    MODKEY|ShiftMask|ControlMask,    XK_m,        setborderpx,    {.i = 2 } },
+	TAGKEYS(                                        XK_1,                        0)
+	TAGKEYS(                                        XK_2,                        1)
+	TAGKEYS(                                        XK_3,                        2)
+	TAGKEYS(                                        XK_4,                        3)
+	TAGKEYS(                                        XK_5,                        4)
+	TAGKEYS(                                        XK_6,                        5)
+	TAGKEYS(                                        XK_7,                        6)
+	TAGKEYS(                                        XK_8,                        7)
+	TAGKEYS(                                        XK_9,                        8)
+	{ KeyPress,    MODKEY|ShiftMask,                XK_q,        quit,           {0} },
 };
 
 /* button definitions */
