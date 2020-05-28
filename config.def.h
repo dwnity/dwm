c5c5a99f0925e9021fe8cf1ca6b0ab89d6b6fb05
Fuseteam <fusekai@outlook.com>
2020-05-28 13:09:23 -0300

    implement monitor rules

--- config.def.h
+++ config.def.h
@@ -3,8 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -31,9 +29,13 @@ static const Rule rules[] = {
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
+static const MonitorRule monrules[] = {
+	/* monitor layout  mfact  nmaster  showbar  topbar */
+	{  1,      0,      0.55,    -1,      1,      1     }, // use a different layout for the second monitor
+	{  -1,     0,      0.55,    1,      1,      1     }, // default
+};
+
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
