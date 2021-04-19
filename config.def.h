23499d8e355eded658f5fb82647573b5a6323f30
Fuseteam <fusekai@outlook.com>
2021-04-19 13:01:26 -0300

    start anbox on tag 7

--- config.def.h
+++ config.def.h
@@ -37,6 +37,7 @@ static const Rule rules[] = {
 	{ "Firefox",    NULL,       NULL,       1 << 8,       0,           -1,     0.95,   1.0 },
 	{ "st",         NULL,       NULL,       0,            1,           -1,     -1,     1.0 },
 	{ "Nextcloud",  NULL,       NULL,       1 << 7,       0,           -1,     -1,     -1 },
+	{ "anbox",      NULL,       NULL,       1 << 6,       0,           -1,     -1,     -1 },
 };
 
 static const MonitorRule monrules[] = {
