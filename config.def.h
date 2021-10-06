e1b93a549a7cea20066e17232e097b7e9a1f9ab5
Fuseteam <fusekai@outlook.com>
2021-10-06 09:35:23 -0300

    more mouse click customizations and shortcuts

--- config.def.h
+++ config.def.h
@@ -30,8 +30,8 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
+	 *      WM_CLASS(STRING) = instance, class
+	 *      WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor opacity inactiveopacity*/
 	{ "Gimp",       NULL,       NULL,       0,            1,           -1,     -1,     -1 },
@@ -62,13 +62,14 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY  Mod4Mask
+#define MODKEY Mod4Mask
 #define AltMask Mod1Mask
+#define NumLock Mod2Mask
 #define TAGKEYS(KEY,TAG) \
-	{ KeyPress,    MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ KeyPress,    MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ KeyPress,    MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ KeyPress,    MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ KeyPress,  MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ KeyPress,  MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ KeyPress,  MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ KeyPress,  MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -77,44 +78,45 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dbash[] = { "dbash", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_forestgreen, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-H", ".bash_history", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_forestgreen, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "gnome-terminal", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *browser[]  = { "firefox", NULL };
 static const char *lanu[] = { "dibus", NULL };
 
 /* custom functions */
 static const char emoji[130]="[ `ibus engine` = uniemoji ] && ibus engine `cat .local/dwm/ibus` || `ibus engine > .local/dwm/ibus && ibus engine uniemoji`";
 static const char *toggleemoji[] = { "/bin/sh", "-c",emoji, NULL };
-static const char *scrot[] = { "/bin/sh", "-c", "cd `cat .config/dwm/scrotdir`;scrot", NULL };
-static const char *scrots[] = { "/bin/sh", "-c", "cd `cat .config/dwm/scrotdir`;scrot -s", NULL };
-static const char *scrotu[] = { "/bin/sh", "-c", "cd `cat .config/dwm/scrotdir`;scrot -u", NULL };
+static const char *scrot[] = { "/bin/sh", "-c", "cd  `cat $HOME/.config/dwm/scrotdir`;scrot `cat $HOME/.config/dwm/scrotname`", NULL };
+static const char *scrots[] = { "/bin/sh", "-c", "cd `cat $HOME/.config/dwm/scrotdir`;scrot -s", NULL };
+static const char *scrotu[] = { "/bin/sh", "-c", "cd `cat $HOME/.config/dwm/scrotdir`;scrot -u", NULL };
 static const char *voldec[] = { "/bin/sh", "-c", "pactl -- set-sink-volume 0 -10%", NULL };
 static const char *volinc[] = { "/bin/sh", "-c", "pactl -- set-sink-volume 0 +10%", NULL };
 static const char *volmut[] = { "/bin/sh", "-c", "amixer -D pulse set Master toggle", NULL };
 
 static Key keys[] = {
-	/*  type       modifier                         key          function        argument */
+	/* type         modifier                        key        function        argument */
 	{ KeyPress,    MODKEY,                          XK_r,        spawn,          {.v = dmenucmd } },
-	{ KeyPress,    MODKEY|ShiftMask,                XK_r,	     spawn,          {.v = termcmd } },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_r,        spawn,          {.v = termcmd } },
 	{ KeyPress,    MODKEY|ControlMask,              XK_r,        spawn,          {.v = dbash } },
-	{ KeyPress,    MODKEY,                          XK_e,        spawn,          {.v = toggleemoji } },
-	{ KeyPress,    MODKEY,                          XK_l,        spawn,          {.v = lanu } },
+	{ KeyPress,    MODKEY,                          XK_e,        spawn,          {.v = toggleemoji} },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_l,        spawn,          {.v = lanu } },
 	{ KeyPress,    MODKEY|ShiftMask,                XK_Return,   spawn,          {.v = browser } },
 	{ KeyPress,    0,                               XK_Print,    spawn,          {.v = scrot } },
 	{ KeyRelease,  ShiftMask,                       XK_Print,    spawn,          {.v = scrots } },
 	{ KeyRelease,  ControlMask,                     XK_Print,    spawn,          {.v = scrotu } },
-	{ KeyPress,    MODKEY|ControlMask,              XK_d,        spawn,	     {.v = voldec} },
-	{ KeyPress,    MODKEY|ControlMask,              XK_i,        spawn,	     {.v = volinc} },
-	{ KeyPress,    MODKEY|ControlMask,              XK_m,        spawn,	     {.v = volmut} },
+	{ KeyPress,    MODKEY|ControlMask,              XK_d,        spawn,          {.v = voldec} },
+	{ KeyPress,    MODKEY|ControlMask,              XK_i,        spawn,          {.v = volinc} },
+	{ KeyPress,    MODKEY|ControlMask,              XK_m,        spawn,          {.v = volmut} },
 	{ KeyPress,    MODKEY|ShiftMask,                XK_d,        setopacity,     {.f = -0.05} },
 	{ KeyPress,    MODKEY|ShiftMask,                XK_i,        setopacity,     {.f = +0.05} },
 	{ KeyPress,    MODKEY|ShiftMask,                XK_f,        setopacity,     {.f = 2.0} },
+	{ KeyPress,    MODKEY|ShiftMask,                XK_b,        spawn,          SHCMD("nitrogen --restore") },
 	{ KeyPress,    MODKEY,                          XK_b,        togglebar,      {0} },
 	{ KeyPress,    AltMask,                         XK_Tab,      focusstack,     {.i = +1 } },
 	{ KeyPress,    AltMask|ShiftMask,               XK_Tab,      focusstack,     {.i = -1 } },
 	{ KeyPress,    MODKEY,                          XK_i,        incnmaster,     {.i = +1 } },
 	{ KeyPress,    MODKEY,                          XK_d,        incnmaster,     {.i = -1 } },
-	{ KeyPress,    MODKEY,	                        XK_k,        setcfact,       {.f = +0.10} },
-	{ KeyPress,    MODKEY,	                        XK_j,        setcfact,       {.f = -0.10} },
+	{ KeyPress,    MODKEY,                          XK_k,        setcfact,       {.f = +0.10} },
+	{ KeyPress,    MODKEY,                          XK_j,        setcfact,       {.f = -0.10} },
 	{ KeyPress,    MODKEY,                          XK_h,        setmfact,       {.f = -0.05} },
 	{ KeyPress,    MODKEY,                          XK_l,        setmfact,       {.f = +0.05} },
 	{ KeyPress,    MODKEY,                          XK_Return,   zoom,           {0} },
@@ -151,6 +153,7 @@ static Key keys[] = {
 };
 
 static const char *powercontrol[] = { "pcon", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_forestgreen, "-sf", col_gray4, NULL };
+static const char *dappo[] = { "dappo", NULL };
 /* custom mouse functions */
 static void switchmain(const Arg *arg);
 
@@ -161,22 +164,24 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,                        Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkLtSymbol,          0,                        Button2,        setlayout,      {.v = &layouts[1]} },
 	{ ClkLtSymbol,          0,                        Button1,        setlayout,      {.v = &layouts[0]} },
-	{ ClkWinTitle,          0,                        Button3,        spawn,          {.v = volinc} },
-	{ ClkWinTitle,          0,                        Button2,        spawn,          {.v = volmut} },
-	{ ClkWinTitle,          0,                        Button1,        spawn,          {.v = voldec} },
-	{ ClkStatusText,        MODKEY|ShiftMask,         Button1,        spawn,          {.v = powercontrol } },
-	{ ClkStatusText,        0,                        Button3,        incnmaster,     {.i = -1 } },
-	{ ClkStatusText,        0,                        Button2,        spawn,          {.v = termcmd } },
-	{ ClkStatusText,        0,                        Button1,        incnmaster,     {.i = +1 } },
+	{ ClkStatusText,        0,                        Button3,        spawn,          {.v = volinc} },
+	{ ClkStatusText,        0,                        Button2,        spawn,          {.v = volmut} },
+	{ ClkStatusText,        0,                        Button1,        spawn,          {.v = voldec} },
+	{ ClkWinTitle,          0,                        Button3,        spawn,          {.v = powercontrol } },
+	{ ClkWinTitle,          MODKEY,                   Button3,        incnmaster,     {.i = +1 } },
+	{ ClkWinTitle,          MODKEY,                   Button2,        spawn,          {.v = termcmd } },
+	{ ClkWinTitle,          MODKEY,                   Button1,        incnmaster,     {.i = -1 } },
+	{ ClkWinTitle,          0,                        Button2,        spawn,          {.v = dappo } },
+	{ ClkWinTitle,          0,                        Button1,        spawn,          {.v = dmenucmd } },
 	{ ClkClientWin,         MODKEY|ControlMask,       Button3,        tagmon,         {.i = +1 } },
 	{ ClkClientWin,         MODKEY|ControlMask,       Button2,        tag,            {.ui = ~0 } },
 	{ ClkClientWin,         MODKEY|ControlMask,       Button1,        tagmon,         {.i = -1 } },
+	{ ClkClientWin,         MODKEY|ShiftMask,         Button3,        setopacity,     {.f = 1.0} },
 	{ ClkClientWin,         MODKEY|ShiftMask,         Button2,        killclient,     {0} },
 	{ ClkClientWin,         MODKEY|ShiftMask,         Button1,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,                   Button3,        resizemouse,    {0} },
 	{ ClkClientWin,         MODKEY,                   Button2,        zoom,           {0} },
 	{ ClkClientWin,         MODKEY,                   Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY|ShiftMask,         Button3,        setopacity,     {.f = 1.0} },
 	{ ClkTagBar,            0,                        Button3,        switchmain,     {0} },
 	{ ClkTagBar,            0,                        Button2,        view,           {0} },
 	{ ClkTagBar,            0,                        Button1,        toggleview,     {0} },
